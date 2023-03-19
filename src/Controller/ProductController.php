569ebc7afb9bfbd2e5f55f9731bd0768bb2dabeb
anrukavina <antunrukavina@hotmail.com>
2023-03-19 22:20:16 +0100

    Add Twig

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,16 +4,34 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): JsonResponse
+    #[Route('/products', name: 'app_product')]
+    public function showProducts(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProductController.php',
+        $url = 'https://dummyjson.com/products';
+
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $url,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_RETURNTRANSFER => true
+        ]);
+
+        $rawResponse = curl_exec($curl);
+        $info = curl_getinfo($curl);
+        curl_close($curl);
+
+        if ($info['http_code'] === 200) {
+            $response = json_decode($rawResponse, true);
+            $products = $response['products'];
+        }
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products ?? [],
         ]);
     }
 }
